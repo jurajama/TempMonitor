5584287902abfa3b96c9767f4c6b9f83e8caa954
jurajama <juha.z.rajamaki@teliacompany.com>
2017-12-27 16:44:18 +0200

    first commit

---
+++ update-roles.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install and/or update all roles
+# --force is needed to do an update of installed roles :-/
+
+ansible-galaxy install -r requirements.yml \
+  -p ext-roles \
+  --force \
+  --verbose
