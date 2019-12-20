eb8f25c1ec4dc2f059d86290f41b1634000fa868
Juha Rajamaki <jurajama@hotmail.com>
2019-12-20 16:08:48 +0200

    Update Raspberry temperature sensor reading code

---
+++ scripts/my_config.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+INFLUXDB_HOST = "your.influxdb.hostname.here"
+INFLUXDB_PORT = 8086
+INFLUXDB_USER = "raspi"
+INFLUXDB_PWD  = "raspi123"
+INFLUXDB_DATABASE = "raspidata"
+
--- scripts/raspi_temp_client.py
+++ scripts/raspi_temp_client.py
@@ -1,42 +1,65 @@
 from influxdb import InfluxDBClient
-import Adafruit_DHT
-import datetime
+import board
+import adafruit_dht
+import datetime, sys, time
+
+import my_config as conf
 
 # This script reads temperature data from temperature sensor and
 # sends data to InfluxDB in cloud
 # See driver installation instructions in:
-# https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/software-install-updated
+# https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/python-setup
 
+def sendDataToInflux(temperature, humidity):
+    client = InfluxDBClient(conf.INFLUXDB_HOST, conf.INFLUXDB_PORT, conf.INFLUXDB_USER, conf.INFLUXDB_PWD, conf.INFLUXDB_DATABASE, timeout=5)
 
-# Primary server
-client = InfluxDBClient('Add-DB-IP-here', 8086, 'raspi', 'raspi123', 'raspidata', timeout=5)
+    timestamp = datetime.datetime.utcnow()
+    str_timestamp = timestamp.isoformat("T") + "Z"
 
-sensor = 22  # sensor model
-pin    = 4   # GPIO pin#4
-
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
-
-timestamp = datetime.datetime.utcnow() # <-- get time in UTC
-str_timestamp = timestamp.isoformat("T") + "Z"
-
-json_temp = [
-    {
-        "measurement": "temps",
-        "tags": {
-            "sensorId": "1"
-        },
-        "time": str_timestamp,
-        "fields": {
-           "temperature": temperature,
-           "humidity": humidity
+    json_temp = [
+        {
+            "measurement": "temps",
+            "tags": {
+                "sensorId": "1"
+            },
+            "time": str_timestamp,
+            "fields": {
+            "temperature": temperature,
+            "humidity": humidity
+            }
         }
-    }
-]
+    ]
 
-client.write_points(json_temp)
+    client.write_points(json_temp)
+
+
+###########################################
+# Main function
+###########################################
+
+if __name__ == '__main__':
+
+  gpio_pin    = board.D4   # GPIO pin#4
+  dhtDevice   = adafruit_dht.DHT22(gpio_pin)
+
+  read_count = 0
+
+  while(read_count<3):
+      try:
+          read_count += 1
+
+          temperature = dhtDevice.temperature
+          humidity    = dhtDevice.humidity
+
+          if humidity is not None and temperature is not None:
+              print('Temp={0:0.1f} C  Humidity={1:0.1f}%'.format(temperature, humidity))
+              sendDataToInflux(temperature, humidity)
+              sys.exit(0)
+          else:
+              print('Failed to get reading from sensor.')
+
+      except RuntimeError as error:
+          # Errors happen fairly often, DHT's are hard to read, just keep going
+          print(error.args[0])
+
+      time.sleep(2)
